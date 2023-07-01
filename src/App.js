eccf6ea5a7e06dc4c56e0ea75f9e3c120b745991
vaibhav <vaibhavkrpandey111@gmail.com>
2023-07-01 19:03:20 +0530

    Updated Lec1 Code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+// object destructuring
+import React, { Component } from 'react';
+// Alias
+import Pizza from './components/Pizza.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// Class based components 
+// inheritence 
+class App extends Component{
+
+  state = {
+        // pizzaPlaces: []
+        dynamicName: "Pizza Hunter", 
+        // brand: "Pizza",
+        // description: "best pizza in town"
+        pizzaPlaces: [
+          { name: "Pizza Hut", description: "Best Pizza", id: 1}, 
+          { name: "Dominoes", description: "Best Pizza in town", id: 2}, 
+          { name: "Papa Johns", description: "Pizza place", id: 3}, 
+          { name: "Ovenbricks", description: "authentic pizza", id: 4},
+          { name: "Ovenbricks", description: "authentic pizza", id: 5},
+          { name: "Ovenbricks", description: "authentic pizza", id: 6},
+        ]
+    }
+
+
+  deleteCard = (id) => {
+        console.log(id);
+        // this.state.dynamicName // don't change the state directly
+        let pizzaPlaces = this.state.pizzaPlaces.filter( (pizzaPlace) => {
+          return pizzaPlace.id !== id
+        } )
+        this.setState({
+            pizzaPlaces: pizzaPlaces
+        })
+        // [].push(23);
+
+        // rendering problem
+        // this.state.pizzaPlaces = pizzaPlaces;
+    }
+
+  render(){
+    return(
+      <div>
+        <h1>{this.state.dynamicName}</h1>        
+        {/* <Pizza brand={this.state.pizzaPlaces[3].name} description={this.state.pizzaPlaces[2].description}></Pizza> */}
+        <Pizza pizzaPlaces={this.state.pizzaPlaces} deleteCard={this.deleteCard}></Pizza>    
+      </div>
+    
+    )
+  }
+
 }
 
+// ES6 
 export default App;
